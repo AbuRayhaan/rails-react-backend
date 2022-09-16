c92c4ef8ad1a0ec01d3e0a0d46204614f000bbdc
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-16 09:13:49 +0100

    Added greetings to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting: 'Hello')
+Greeting.create(greeting: 'Aku Owuro')
+Greeting.create(greeting: 'Inna Kwana')
+Greeting.create(greeting: 'Ibolaji')
+Greeting.create(greeting: 'Tutoma')
